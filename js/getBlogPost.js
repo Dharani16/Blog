b80474312a3abd9f7f9ca62ffb5f1a0081769e36
Dharani <rdharani1606@gmail.com>
2017-09-06 14:57:33 +0530

    fixing comment issues

--- js/getBlogPost.js
+++ js/getBlogPost.js
@@ -8,6 +8,8 @@ var postRecentId ;
 
 $(document).ready(function(){
 	callIndex();
+	$("#formDisplay").empty();
+	$("#commentsDisplay").empty();
 });
 
 //callback homepage
@@ -19,13 +21,15 @@ function callIndex(){
 			$("#blogPostDisplay").html(template);
 		});
 	});
+	$("#formDisplay").empty();
+	$("#commentsDisplay").empty();
 }
 
 //callback readmore button
 function readMoreButton(id){
 	console.log("postID = "+id);
 	postRecentId = id;
-	$("#blogPostDisplay").html('<div class = "container"><img id="theImg" src="images/blogBanner.jpg" width="1170px" height="200px" /> </div>');
+	$("#blogPostDisplay").html('<div class = "container"><img id="theImg" src="images/blogBanner.jpg" width="1160px;" height="200px" /> </div>');
 	var READMORE_URL = COMMENT_FIRST + id;
 	$.get('template/readMore.html',function(readMoreTemplate){
 		$.getJSON(READMORE_URL,function(readMoreData){
@@ -33,54 +37,52 @@ function readMoreButton(id){
 			var readPost = readMoreData.body;
 			template = _.template(readMoreTemplate,{variable:'rmVars'})({re:readTitle,reb:readPost});
 			$("#blogPostDisplay").append(template);	
-			cmtDisplay();					
+			commentFormDesign();
+			commentDisplay();
 		});
 	});
 }
-/*
-//callback homepage
-function callIndex(){
-	$.get('template/header.html',function(templates){
-		$.getJSON(GET_URL,function(data){
-			var result = data;
-			template = _.template(templates,{variable:'vars'})({res:result});
-			$("#blogPostDisplay").html(template);
-		});
-	});
-}
-*/ 
-//comment form design
-function cmtDisplay(){
-   $.get('template/commentDisplay.html',function(cmtDisplayTemplate){
-   		//console.log(cmtDisplayTemplate);
-   		var COMMENT_URL = COMMENT_FIRST + postRecentId + COMMENT_LAST + COMMENT_SORT;
-   		$.getJSON(COMMENT_URL,function(cmtData){
-			//console.log("Comment URL = "+COMMENT_URL);
+
+//callback comment display
+function commentDisplay(){
+	$.get('template/cmtDisplay.html',function(cmtDisp){
+		var COMMENT_URL = COMMENT_FIRST + postRecentId + COMMENT_LAST + COMMENT_SORT;
+		$.getJSON(COMMENT_URL,function(cmtData){
 			var cmtResult = cmtData;
-			console.log("Comment content");
-			console.dir(cmtResult);
-			template = _.template(cmtDisplayTemplate,{variable:'cmtVars'})({res:"Dharani dharan"});
-			$("#blogPostDisplay").append(template);
+			var rows = "";
+			rows += _.template(cmtDisp,{variable:'data'})({resCmt:cmtResult});
+			$("#commentsDisplay").html(rows);
 		});
-   }); 	
+	});
 }
 
-
-//comment displayed 
-// function cmtDisplay(){
-// 	$.get('template/commentDisplay.html',function(cmtDisplayData){
-// 		console.log(cmtDisplayData);
-// 	});
-// 	var COMMENT_URL = COMMENT_FIRST + postRecentId + COMMENT_LAST + COMMENT_SORT;
-// 	$.getJSON(COMMENT_URL,function(postComment){
-// 	console.log("cmtFormDisplay = "+COMMENT_URL);
-// 		$.each(postComment,function(key,value){
-// 			$("#blogPostDisplay").append(cmtSection);
-
-// 		});
-// 	});
-// }
-
+//callback commnet form 
+function commentFormDesign(){
+	$.get('template/cmtForm.html',function(cmtFormTemplate){
+		template = _.template(cmtFormTemplate);
+		$("#formDisplay").html(template);
+		//submit comment
+		$("#btnCmtSubmit").click(function(){
+			var name = $("#cmName").val();
+  	 		var body = $("#cmMessages").val();
+ 		 	var email = $("#cmEmail").val(); 
+	 	 	console.log("Name : "+name+"Email : "+email+"PostId : "+postRecentId+"Body : "+body);
+	        $.ajax(COMMENT_URL_DATA, {	            	
+	            method: 'POST',
+	            data: {
+	          	    name: name,
+	              	body:  body,
+	                email: email,
+	                postId: postRecentId
+	            }
+	        }).then(function(data) {
+	           	alert("Comment sent successfully !!");
+	           	$("input[type=text], textarea").val("");
+	           	commentDisplay();
+	        }); 	        
+		});
+	});
+}
 
 // about callback function
 function callAboutUs(){
@@ -88,6 +90,8 @@ function callAboutUs(){
 		template = _.template(data);
 		$("#blogPostDisplay").html(template);
 	});
+	$("#formDisplay").empty();
+	$("#commentsDisplay").empty();
 }
 
 // contact callback function
@@ -96,6 +100,8 @@ function callContactUs(){
 		template = _.template(data);
 		$("#blogPostDisplay").html(template);
 	});
+	$("#formDisplay").empty();
+	$("#commentsDisplay").empty();
 }
 
 //callback create post
@@ -118,82 +124,8 @@ function callNewPostId(){
 	            }
 	        }).then(function(data) {
 	           	alert("Inserted new blog post successfully !!");
-	        }); 	
+	           	$("input[type=text], textarea").val("");
+	        }); 
 		});			
 	});
 }
-
-/*
-//callback readmore button
-function readMoreButton(id){
-	console.log("Read more postID = "+id);
-	postRecentId = id;
-	$("#blogPostDisplay").html('<img id="theImg" src="images/blogBanner.jpg" width="1170px" height="200px" />');
-	var READMORE_URL = COMMENT_FIRST + id;
-	$.getJSON(READMORE_URL,function(data){
-		$("#blogPostDisplay").append("<h2 id='theHeading'>" +data.title+ "</h2>");
-		$("#blogPostDisplay").append("<p id='theParagraph'>" +data.body+ "</p>");
-		$("#blogPostDisplay").append("<hr/>");
-		//get comment
-		commentDesign();
-		$("#blogPostDisplay").append("<hr/>");
-		var COMMENT_URL = COMMENT_FIRST + id + COMMENT_LAST + COMMENT_SORT;
-		$.getJSON(COMMENT_URL,function(postComment){
-			console.log("cmtFormDisplay = "+COMMENT_URL);
-			$.each(postComment,function(key,value){
-				$("#blogPostDisplay").append('<div class="thmbImage" style="float:left;margin-right:15px;"><img id="theImg" src="images/userIcon.png" width="56px" height="56px" style="overflow:auto;"/></div>');
-				$("#blogPostDisplay").append("<div class='description' style='overflow:auto;'><h5 id='commentName'>" +postComment[key].name+ "</h5><p id='commentMessage'>" +postComment[key].body+ "</p></div>");
-				$("#blogPostDisplay").append("<hr/>");
-			});
-		});
-	});
-}
-
-
-function readMoreButton(id){
-	console.log("Read more postID = "+id);
-	postRecentId = id;
-	$("#blogPostDisplay").html('<div class = "container"><img id="theImg" src="images/blogBanner.jpg" width="1170px" height="200px" /> </div>');
-	var READMORE_URL = COMMENT_FIRST + id;
-	$.getJSON(READMORE_URL,function(data){
-		var blogContent = '<div class="container">';
-		blogContent += '<h2 id="theHeading">' +data.title+ '</h2>';
-		blogContent += '<p id="theParagraph">'+data.body+'</p>';
-		blogContent += '</div>';
-		blogContent += '<hr/>';
-		$("#blogPostDisplay").append(blogContent);		
-		//get comment
-		commentDesign();
-		$("#blogPostDisplay").append("<hr/>");
-		cmtDisplay();
-		//$("#blogPostDisplay").append("<div id='cmtDisplayRefresh'> Welcome screen </div>")		
-	});
-}
-
-
-//comment displayed 
-function cmtDisplay(){
-	var COMMENT_URL = COMMENT_FIRST + postRecentId + COMMENT_LAST + COMMENT_SORT;
-	$.getJSON(COMMENT_URL,function(postComment){
-	console.log("cmtFormDisplay = "+COMMENT_URL);
-		$.each(postComment,function(key,value){
-			var cmtSection = '<div class = "container" >';
-			cmtSection += '<div id="cmtSectionPart">'
-			cmtSection += '<div class="thmbImage" style="float:left;margin-right:15px;">';
-			cmtSection += '<img id="theImg" src="images/userIcon.png" width="56px" height="56px" style="overflow:auto;"/>';
-			cmtSection += '</div>';
-			cmtSection += '<div class="description" style="overflow:auto;">';
-			cmtSection += '<h5 id="commentName"> '+postComment[key].name+' </h5>';
-			cmtSection += '<p id="commentMessage">'+postComment[key].body+'</p>';
-			cmtSection += '</div>';
-			cmtSection += '</div>';
-			cmtSection += '</div>';
-			cmtSection += '<hr/>';
-			//$("#blogPostDisplay").append('<div class="thmbImage" style="float:left;margin-right:15px;"><img id="theImg" src="images/userIcon.png" width="56px" height="56px" style="overflow:auto;"/></div>');
-			//$("#blogPostDisplay").append("<div class='description' style='overflow:auto;'><h5 id='commentName'>" +postComment[key].name+ "</h5><p id='commentMessage'>" +postComment[key].body+ "</p></div>");
-			$("#blogPostDisplay").append(cmtSection);
-		});
-	});
-}
-
-*/ 
